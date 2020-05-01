0c7d57f13b5db0a00b3b4432d034f28508e32fdc
宋天琢 <lmsongtz@foxmail.com>
2020-05-01 17:03:50 +0800

    Add files via upload
    nimEGE

---
+++ ege.nim
@@ -0,0 +1,23 @@
+#[
+    ege graphics library wrapper
+    complie command:
+        nim cpp ege.nim
+]#
+
+{.passL: "-lgraphics -lgdi32 -limm32 -lmsimg32 -lole32 -loleaut32 -lwinmm -luuid".}
+
+proc closegraph*(){.importc:"closegraph", header:"graphics.h", cdecl.}
+proc initgraph*(Width: cint; Height: cint; Flag: cint = 0x100){.cdecl, importc:"initgraph", header:"graphics.h", exportc, dynlib.}
+proc initgraph*(gdriver: ptr cint; gmode: ptr cint; path: cstring) {.importc:"initgraph", header:"graphics.h", cdecl, exportc, dynlib.}
+proc getch*(){.importc:"getch", header:"graphics.h", cdecl.}
+
+#[
+
+to do 
+
+]#
+
+when isMainModule:
+    initgraph(640,480)
+    getch()
+    closegraph()
