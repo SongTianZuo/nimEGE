ce9eca10365125433f7b24ce867d8486517b9cee
������ <lmsongtz@foxmail.com>
2020-05-03 13:21:49 +0800

    colorApi,!unkown Bug!Nim?

--- ege.nim
+++ ege.nim
@@ -3,7 +3,7 @@
 ##  使用前，先将 EGE 的头文件（graphics.h, ege.h, ege文件夹) 和 
 ##  对应的静态链接（libgraphics64.a 或 libgraphics.a）安装配置到MingW64/32中
 ##
-##  before use, make sure EGE header files(graphics.h, ege.h, ege folder) and the 
+##  before using, make sure EGE header files(graphics.h, ege.h, ege folder) and the 
 ##  corresponding static library(libgraphics64.a or libgraphics.a) copy into MingW64/32
 
 #[
@@ -19,16 +19,94 @@ else:
 
 const
     CW_USEDEFAULT* = (cast[cint](0x80000000))
+    SHOWCONSOLE* = 1
+
+template RGBTOBGR*(color: untyped): untyped =
+  ((((color) and 0x000000FF) shl 16) or (((color) and 0x00FF0000) shr 16) or
+      ((color) and 0xFF00FF00))
+
+template EGERGB*(r, g, b: untyped): untyped =
+    ## EGERGB 宏用于通过红、绿、蓝颜色分量合成颜色。
+    ## 
+    ## The EGERGB macro is used to synthesize colors through red, green, and blue color components.
+    ## 
+    (((r) shl 16) or ((g) shl 8) or (b))
+
+template EGERGBA*(r, g, b, a: untyped): untyped =
+  (((r) shl 16) or ((g) shl 8) or (b) or ((a) shl 24))
+
+template EGEARGB*(a, r, g, b: untyped): untyped =
+  (((r) shl 16) or ((g) shl 8) or (b) or ((a) shl 24))
+
+template EGEACOLOR*(a, color: untyped): untyped =
+  (((color) and 0x00FFFFFF) or ((a) shl 24))
+
+template EGECOLORA*(color, a: untyped): untyped =
+  (((color) and 0x00FFFFFF) or ((a) shl 24))
+
+template EGEGET_R*(c: untyped): untyped =
+    ## EGEGET_R 宏用于返回指定颜色中的红色值。值的范围 0~255。
+    ## 
+    ## The EGEGET_R macro is used to return the red value in the specified color.Values range from 0 to 255.
+    ## 
+    (((c) shr 16) and 0x000000FF)
+
+template EGEGET_G*(c: untyped): untyped =
+    ## EGEGET_G 宏用于返回指定颜色中的绿色值。值的范围 0~255。
+    ## 
+    ## The EGEGET_G macro is used to return the green value in the specified color.Values range from 0 to 255.
+    ## 
+    (((c) shr 8) and 0x000000FF)
+
+template EGEGET_B*(c: untyped): untyped =
+    ## EGEGET_B 宏用于返回指定颜色中的蓝色值。值的范围 0~255。
+    ## 
+    ## The EGEGET_B macro is used to return the blue value in the specified color. Values range from 0 to 255.
+    ## 
+    (((c)) and 0x000000FF)
+
+template EGEGET_A*(c: untyped): untyped =
+  (((c) shr 24) and 0x000000FF)
+
+template EGEGRAY*(gray: untyped): untyped =
+  (((gray) shl 16) or ((gray) shl 8) or (gray))
+
+template EGEGRAYA*(gray, a: untyped): untyped =
+  (((gray) shl 16) or ((gray) shl 8) or (gray) or ((a) shl 24))
+
+template EGEAGRAY*(a, gray: untyped): untyped =
+  (((gray) shl 16) or ((gray) shl 8) or (gray) or ((a) shl 24))
+
 type
     IMAGE = object
     PIMAGE* = ptr IMAGE
+    
     rendermode_e* = enum
         RENDER_AUTO, RENDER_MANUAL
+        
     initmode_flag* = enum
         INIT_DEFAULT = 0x00000000, INIT_NOBORDER = 0x00000001, INIT_CHILD = 0x00000002,
         INIT_TOPMOST = 0x00000004, INIT_RENDERMANUAL = 0x00000008,
         INIT_NOFORCEEXIT = 0x00000010, INIT_WITHLOGO = 0x00000100,
         INIT_ANIMATION = 0x00000118
+        
+    COLORS* = enum
+        BLACK = 0, BLUE = EGERGB(0, 0, 0x000000A8), GREEN = EGERGB(0, 0x000000A8, 0),
+        CYAN = EGERGB(0, 0x000000A8, 0x000000A8), 
+        DARKGRAY = EGERGB(0x00000054, 0x00000054, 0x00000054),
+        LIGHTBLUE = EGERGB(0x00000054, 0x00000054, 0x000000FC),
+        LIGHTGREEN = EGERGB(0x00000054, 0x000000FC, 0x00000054),
+        LIGHTCYAN = EGERGB(0x00000054, 0x000000FC, 0x000000FC),
+        RED = EGERGB(0x000000A8, 0, 0),
+        MAGENTA = EGERGB(0x000000A8, 0, 0x000000A8),
+        BROWN = EGERGB(0x000000A8, 0x000000A8, 0),
+        LIGHTGRAY = EGERGB(0x000000A8, 0x000000A8, 0x000000A8),        
+        LIGHTRED = EGERGB(0x000000FC, 0x00000054, 0x00000054),
+        LIGHTMAGENTA = EGERGB(0x000000FC, 0x00000054, 0x000000FC),
+        YELLOW = EGERGB(0x000000FC, 0x000000FC, 0x00000054),
+        WHITE = EGERGB(0x000000FC, 0x000000FC, 0x000000FC)
+        
+    color_t* = cuint
 
 # environment
 # 绘图环境相关函数
@@ -392,6 +470,314 @@ proc window_setviewport*(left: cint; top: cint; right: cint; bottom: cint)  {.im
     ##  bottom: the lower y coordinate of the visible region.(right-1, bottom-1) is the coordinate at the bottom right corner of the view.
     ## 
 
+# colors
+# 颜色表示及相关函数
+proc getbkcolor*(pimg: PIMAGE = nil): color_t {.importc, header:"graphics.h", cdecl.}
+    ## 这个函数用于获取当前绘图背景色。
+    ## 
+    ## This function is used to get the current drawing background color.
+    ## 
+    ## **返回值**
+    ## 
+    ##  返回当前绘图背景色。
+    ## 
+    ## **return**
+    ## 
+    ##  Returns the current drawing background color.
+    ## 
+
+proc getcolor*(pimg: PIMAGE = nil): color_t {.importc, header:"graphics.h", cdecl.}
+    ## 这个函数用于获取当前绘图前景色。
+    ## 
+    ## This function is used to get the current drawing foreground.
+    ## 
+
+proc getfillcolor*(pimg: PIMAGE = nil): color_t {.importc, header:"graphics.h", cdecl.}
+    ## 这个函数用于获取当前绘图填充色。
+    ## 
+    ## This function is used to get the current drawing fill color.
+    ## 
+
+proc hsl2rgb*(H: cfloat; S: cfloat; L: cfloat): color_t {.importc, header:"graphics.h", cdecl.}
+    ## 该函数用于转换 HSL 颜色为 RGB 颜色。
+    ## 
+    ## This function is used to convert HSL color to RGB color.
+    ##
+    ## **参数** 
+    ## 
+    ##  H
+    ## 
+    ##  原 HSL 颜色模型的 Hue(色相) 分量，0 ≤ H ＜ 360。
+    ## 
+    ##  S
+    ## 
+    ##  原 HSL 颜色模型的 Saturation(饱和度) 分量，0 ≤ S ≤ 1。
+    ## 
+    ##  L
+    ## 
+    ##  原 HSL 颜色模型的 Lightness(亮度) 分量，0 ≤ L ≤ 1。
+    ## 
+    ## **parameter**
+    ## 
+    ##  H.
+    ## 
+    ##  Hue(Hue) component of the original HSL color model, 0 ≤ H < 360.
+    ## 
+    ##  s.
+    ## 
+    ##  The Saturation component of the original HSL color model is 0 ≤ S ≤ 1.
+    ## 
+    ##  L
+    ## 
+    ##  The Lightness of an HSL color model (brightness) component, 0 ≤ L ≤ 1。
+    ## 
+    
+    
+proc hsv2rgb*(H: cfloat; S: cfloat; V: cfloat): color_t {.importc, header:"graphics.h", cdecl.}
+    ## 该函数用于转换 HSV 颜色为 RGB 颜色。
+    ## 
+    ## This function is used to convert HSV color to RGB color.
+    ## 
+    ## **参数** 
+    ## 
+    ##  H
+    ## 
+    ##  原 HSL 颜色模型的 Hue(色相) 分量，0 ≤ H ＜ 360。
+    ## 
+    ##  S
+    ## 
+    ##  原 HSL 颜色模型的 Saturation(饱和度) 分量，0 ≤ S ≤ 1。
+    ## 
+    ##  V
+    ## 
+    ##  原 HSL 颜色模型的 Value(明度) 分量，0 ≤ L ≤ 1。
+    ## 
+    ## **parameter**
+    ## 
+    ##  H.
+    ## 
+    ##  Hue(Hue) component of the original HSL color model, 0 ≤ H < 360.
+    ## 
+    ##  s.
+    ## 
+    ##  The Saturation component of the original HSL color model is 0 ≤ S ≤ 1.
+    ## 
+    ##  V
+    ## 
+    ##  The Value(lightness) component of the original HSL color model is 0 ≤ L ≤ 1.
+    ## 
+
+proc rgb2gray*(rgb: color_t): color_t {.importc, header:"graphics.h", cdecl.}
+    ## 该函数用于返回与指定颜色对应的灰度值颜色。
+    ## 
+    ## This function returns the color of the grayscale value corresponding to the specified color.
+    ## 
+    
+proc rgb2gray*(rgb: COLORS): color_t =
+    ## 详见 rgb2gray(rgb: color_t)
+    ##
+    ## see rgb2gray(rgb: color_t)
+    rgb2gray(cast[color_t](rgb))
+proc rgb2hsl*(rgb: color_t; H: ptr cfloat; S: ptr cfloat; L: ptr cfloat) {.importc, header:"graphics.h", cdecl.}
+    ## 该函数用于转换 RGB 颜色为 HSL 颜色。
+    ## 
+    ## This function is used to convert RGB color to HSL color.
+    ## 
+    ## **参数** 
+    ## 
+    ##  rgb
+    ## 
+    ##  原 RGB 颜色。
+    ## 
+    ##  H
+    ## 
+    ##  原 HSL 颜色模型的 Hue(色相) 分量，0 ≤ H ＜ 360。
+    ## 
+    ##  S
+    ## 
+    ##  原 HSL 颜色模型的 Saturation(饱和度) 分量，0 ≤ S ≤ 1。
+    ## 
+    ##  L
+    ## 
+    ##  原 HSL 颜色模型的 Lightness(亮度) 分量，0 ≤ L ≤ 1。
+    ## 
+    ## **parameter**
+    ## 
+    ##  rgb
+    ## 
+    ##  Original RGB color.
+    ## 
+    ##  H.
+    ## 
+    ##  Hue(Hue) component of the original HSL color model, 0 ≤ H < 360.
+    ## 
+    ##  s.
+    ## 
+    ##  The Saturation component of the original HSL color model is 0 ≤ S ≤ 1.
+    ## 
+    ##  L
+    ## 
+    ##  The Lightness of an HSL color model (brightness) component, 0 ≤ L ≤ 1。
+    ## 
+
+proc rgb2hsl*(rgb: COLORS; H: ptr cfloat; S: ptr cfloat; L: ptr cfloat) =
+    ## 详见 rgb2hsl(rgb: color_t; H: ptr cfloat; S: ptr cfloat; L: ptr cfloat)
+    ##
+    ## see rgb2hsl(rgb: color_t; H: ptr cfloat; S: ptr cfloat; L: ptr cfloat)
+    rgb2hsl(cast[color_t](rgb), H, S, L)
+    
+proc rgb2hsv*(rgb: color_t; H: ptr cfloat; S: ptr cfloat; V: ptr cfloat) {.importc, header:"graphics.h", cdecl.}
+    ## 该函数用于转换 RGB 颜色为 HSV 颜色。
+    ## 
+    ## This function is used to convert RGB color to HSV color.
+    ## 
+    ## **参数** 
+    ## 
+    ##  rgb
+    ## 
+    ##  原 RGB 颜色。
+    ## 
+    ##  H
+    ## 
+    ##  原 HSL 颜色模型的 Hue(色相) 分量，0 ≤ H ＜ 360。
+    ## 
+    ##  S
+    ## 
+    ##  原 HSL 颜色模型的 Saturation(饱和度) 分量，0 ≤ S ≤ 1。
+    ## 
+    ##  V
+    ## 
+    ##  原 HSL 颜色模型的 Value(明度) 分量，0 ≤ L ≤ 1。
+    ## 
+    ## **parameter**
+    ## 
+    ##  rgb
+    ## 
+    ##  Original RGB color.
+    ## 
+    ##  H.
+    ## 
+    ##  Hue(Hue) component of the original HSL color model, 0 ≤ H < 360.
+    ## 
+    ##  s.
+    ## 
+    ##  The Saturation component of the original HSL color model is 0 ≤ S ≤ 1.
+    ## 
+    ##  V
+    ## 
+    ##  The Value(lightness) component of the original HSL color model is 0 ≤ L ≤ 1.
+    ## 
+
+proc rgb2hsv*(rgb: COLORS; H: ptr cfloat; S: ptr cfloat; V: ptr cfloat)=
+    ## 详见 rgb2hsv(rgb: color_t; H: ptr cfloat; S: ptr cfloat; V: ptr cfloat)
+    ##
+    ## See rgb2hsv(rgb: color_t; H: ptr cfloat; S: ptr cfloat; V: ptr cfloat)
+    rgb2hsv(cast[color_t](rgb), H, S, V)
+    
+proc setbkcolor*(color: color_t; pimg: PIMAGE = nil) {.importc, header:"graphics.h", cdecl.}
+    ## 这个函数用于设置当前背景色。并且会把当前图片上是原背景色的像素，转变为新的背景色。
+    ## 
+    ## This function is used to set the current background color.And will be the current image is the original background color pixels, the new background color.
+    ## 
+    ## **参数** 
+    ## 
+    ##  color
+    ## 
+    ##  指定要设置的背景颜色。注意，该设置会同时影响文字背景色。
+    ## 
+    ## **parameter**
+    ## 
+    ##  color
+    ## 
+    ##  Specifies the background color to set.Note that this setting also affects the text background color.
+    ## 
+
+proc setbkcolor*(color: COLORS; pimg: PIMAGE = nil)=
+    ## 详见 setbkcolor(color: color_t; pimg: PIMAGE = nil)
+    ## 
+    ## See setbkcolor(color: color_t; pimg: PIMAGE = nil)
+    setbkcolor(cast[color_t](color), pimg)
+    
+proc setbkcolor_f*(color: color_t; pimg: PIMAGE = nil) {.importc, header:"graphics.h", cdecl.}
+    ## 设置清屏时所用的背景色。
+    ## 即仅设置cleardevice时所使用的颜色，不立即生效，需要等cleardevice调用。 
+    ##
+    ## Sets the background color to use when clearing the screen。
+    ## That is, only the color used when setting cleardevice does not take effect immediately, so you need to wait for cleardevice call.
+    ##
+    
+proc setbkcolor_f*(color: COLORS; pimg: PIMAGE = nil)=
+    ## 详见 setbkcolor_f(color: color_t; pimg: PIMAGE = nil)
+    ## 
+    ## See setbkcolor_f(color: color_t; pimg: PIMAGE = nil)
+    setbkcolor_f(cast[color_t](color), pimg)
+
+proc setbkmode*(iBkMode: cint; pimg: PIMAGE = nil) {.importc, header:"graphics.h", cdecl.}
+    ## 这个函数用于设置输出文字时的背景模式。
+    ## 
+    ## This function sets the background mode for the output text.
+    ## 
+    ## **参数** 
+    ## 
+    ##  iBkMode
+    ## 
+    ##  指定输出文字时的背景模式，可以是以下值：
+    ## 
+    ##   OPAQUE值：	背景用当前背景色填充（默认）。
+    ## 
+    ##   TRANSPARENT：	背景是透明的。
+    ## 
+    ## **parameter**
+    ## 
+    ##  iBkMode
+    ## 
+    ##  Specifies the background mode for the output text, which can be the following:
+    ## 
+    ##   OPAQUE：   The background is filled with the current background color (default).
+    ## 
+    ##   TRANSPARENT： The background is transparent.
+    ## 
+
+proc setcolor*(color: color_t; pimg: PIMAGE = nil) {.importc, header:"graphics.h", cdecl.}
+    ## 这个函数用于设置绘图前景色。
+    ## 
+    ## This function is used to set the drawing foreground.
+    ## 
+    
+proc setcolor*(color: COLORS; pimg: PIMAGE = nil)=
+    ## 详见 setcolor(color: color_t; pimg: PIMAGE = nil)
+    ##
+    ## See setcolor(color: color_t; pimg: PIMAGE = nil)
+    setcolor(cast[color_t](color), pimg)
+    
+proc setfillcolor*(color: color_t; pimg: PIMAGE = nil) {.importc, header:"graphics.h", cdecl.}
+    ## 这个函数用于设置绘图填充色。
+    ## 
+    ## This function is used to set the drawing fill color.
+    ## 
+    
+proc setfillcolor*(color: COLORS; pimg: PIMAGE = nil)=
+    ## 详见 setfillcolor(color: color_t; pimg: PIMAGE = nil)
+    ##
+    ## See setfillcolor(color: color_t; pimg: PIMAGE = nil)
+    setfillcolor(cast[color_t](color), pimg)
+    
+proc setfontbkcolor*(color: color_t; pimg: PIMAGE = nil) {.importc, header:"graphics.h", cdecl.}
+    ## 这个函数用于设置文字背景色。
+    ## 
+    ## This function is used to set the text background color.
+    ## 
+    
+proc setfontbkcolor*(color: COLORS; pimg: PIMAGE = nil)=
+    ## 详见 setfontbkcolor(color: color_t; pimg: PIMAGE = nil)
+    ##
+    ## See setfontbkcolor(color: color_t; pimg: PIMAGE = nil)
+    setfontbkcolor(cast[color_t](color), pimg)
+
+
+    
+
+
 proc getch*():cint{.importc:"getch", header:"graphics.h", cdecl.}
     ## 这个函数用于获取键盘字符输入，如果当前没有输入，则等待。
     ## 
@@ -405,10 +791,8 @@ proc getch*():cint{.importc:"getch", header:"graphics.h", cdecl.}
     ## 
     ##  If there is keyboard character input, return the key code;Otherwise do not return and wait.
     ## 
-
-
+    
 #[
-
 to do：
 待完成:
 
@@ -417,7 +801,15 @@ to do：
 when isMainModule:
     initgraph(640,480)
     setcaption("windows Title")
+    setcolor(0xff0000);
+    setcolor(BLUE)
+    setcolor(EGERGB(0, 0, 255))
+    setcolor(hsl2rgb(240, 1, 0.5))
+    setbkcolor_f(RED)
+    cleardevice()
     discard getch()
     setcaption( newWideCString("窗口标题"))
+    setbkcolor_f(YELLOW)
+    cleardevice()
     discard getch()
     closegraph()
